f6be2af2d881842d094387f0f31d6be8df43cc64
Henry <henryppercy@gmail.com>
2022-12-01 20:42:34 +0000

    Set client side routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.scss'
+import Home from './Pages/Home';
+import Products from './Pages/Products';
+import Product from './Pages/Product';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-      <h1>Test</h1>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Product />} />
+      <Route path="/product" element={<Products />} />
+    </Routes>
+  );
 }
 
 export default App
