91faf68983e89ba8ce4db3aac82cd3d205a88c31
Henry <henryppercy@gmail.com>
2022-12-02 14:56:51 +0000

    Wrote async function to fetch all products on mount, dynamically rendered product cards

--- src/Pages/Products/index.tsx
+++ src/Pages/Products/index.tsx
@@ -1,82 +1,67 @@
+import { useEffect, useState } from "react";
 import Navbar from "../../Componants/Navbar";
-import test from '../../assets/test.jpeg';
+import ProductCard from "../../Componants/ProductCard";
+import errorHandler from "../../utils/errorHandler";
 import './Products.scss';
 
+type Product = {
+  id: number,
+  title: string,
+  price: number,
+  image: string,
+}
+
 function Products() {
-    return (
-      <>
-        <Navbar></Navbar>
-        <section className="products-page">
-          <form>
-              <label htmlFor="categories"></label>
-              <select name="categories" id="categories">
-                <option value="category" disabled selected>Category</option>
-                <option value="aprons">Aprons</option>
-                <option value="baseball-ats">Baseball Hats</option>
-                <option value="mugs">Mugs</option>
-                <option value="t-shits">T-Shits</option>
-              </select>
-              <label htmlFor="characters"></label>
-              <select name="characters" id="characters">
-                <option value="character" disabled selected>Character</option>
-                <option value="bubbles">Bubbles</option>
-                <option value="dolores">Dolores</option>
-                <option value="fred">Fred</option>
-                <option value="rex">Rex</option>
-              </select>
-          </form>
-            
-          <div className="products">
-            <div className="card">
-              <img src={test}></img>
-              <h4>It’s All 0’s and 1’s to Me! Apron</h4>
-              <p>$24.00</p>
-            </div>
-            <div className="card">
-              <img src={test}></img>
-              <h4>It’s All 0’s and 1’s to Me! Apron</h4>
-              <p>$24.00</p>
-            </div>
-            <div className="card">
-              <img src={test}></img>
-              <h4>It’s All 0’s and 1’s to Me! Apron</h4>
-              <p>$24.00</p>
-            </div>
-            <div className="card">
-              <img src={test}></img>
-              <h4>It’s All 0’s and 1’s to Me! Apron</h4>
-              <p>$24.00</p>
-            </div>
-            <div className="card">
-              <img src={test}></img>
-              <h4>It’s All 0’s and 1’s to Me! Apron</h4>
-              <p>$24.00</p>
-            </div>
-            <div className="card">
-              <img src={test}></img>
-              <h4>It’s All 0’s and 1’s to Me! Apron</h4>
-              <p>$24.00</p>
-            </div>
-            <div className="card">
-              <img src={test}></img>
-              <h4>It’s All 0’s and 1’s to Me! Apron</h4>
-              <p>$24.00</p>
-            </div>
-            <div className="card">
-              <img src={test}></img>
-              <h4>It’s All 0’s and 1’s to Me! Apron</h4>
-              <p>$24.00</p>
-            </div>
-            <div className="card">
-              <img src={test}></img>
-              <h4>It’s All 0’s and 1’s to Me! Apron</h4>
-              <p>$24.00</p>
-            </div>
-          </div>
-        </section>
-      </>
-    );
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    getAllProducts()
+  }, [])
+  
+  const getAllProducts = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/products');
+      if (await errorHandler(response, setError)) {
+        const data = await response.json();
+        setProducts(data.data);
+      }
+    } catch {
+      setError('Unable to retrieve products');
+    }
   }
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <section className="products-page">
+        <form>
+            <label htmlFor="categories"></label>
+            <select name="categories" id="categories">
+              <option value="category" disabled selected>Category</option>
+              <option value="aprons">Aprons</option>
+              <option value="baseball-ats">Baseball Hats</option>
+              <option value="mugs">Mugs</option>
+              <option value="t-shits">T-Shits</option>
+            </select>
+            <label htmlFor="characters"></label>
+            <select name="characters" id="characters">
+              <option value="character" disabled selected>Character</option>
+              <option value="bubbles">Bubbles</option>
+              <option value="dolores">Dolores</option>
+              <option value="fred">Fred</option>
+              <option value="rex">Rex</option>
+            </select>
+        </form>
+          
+        <div className="products">
+          {error && <div className="error">Error: {error}</div>}
+          {products.map((product: Product) => <ProductCard key={product.id} title={product.title} price={product.price} image={product.image}></ProductCard>)}
+        </div>
+      </section>
+    </>
+  );
+}
   
   export default Products;
   
