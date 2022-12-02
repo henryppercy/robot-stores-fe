74906c669b506ff7111c4d14560c6dee4cf7e58b
Henry <henryppercy@gmail.com>
2022-12-02 14:54:46 +0000

    Added dynamic route to products

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Products />} />
-      <Route path="/product" element={<Product />} />
+      <Route path="/product/:id" element={<Product />} />
     </Routes>
   );
 }
