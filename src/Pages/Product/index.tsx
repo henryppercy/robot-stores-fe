2caa49a3e51e807df5dd9c59578b45be565f9497
Henry <henryppercy@gmail.com>
2022-12-02 08:51:51 +0000

    Created template pages

---
+++ src/Pages/Product/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../../Componants/Navbar";
+
+function Product() {
+    return (
+      <>
+        <Navbar></Navbar>
+      </>
+    )
+  }
+  
+  export default Product;
+  
---
+++ src/Pages/Products/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../../Componants/Navbar";
+
+function Products() {
+    return (
+      <>
+        <Navbar></Navbar>
+      </>
+    )
+  }
+  
+  export default Products;
+  
