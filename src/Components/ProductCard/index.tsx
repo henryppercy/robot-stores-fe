c60ae24a067dcc8f75794392a90327af051d991f
Henry percy <henryppercy@gmail.com>
2023-01-05 16:44:39 +0000

    Changed to arrow syntax and removed whitespace

--- src/Components/ProductCard/index.tsx
+++ src/Components/ProductCard/index.tsx
@@ -7,15 +7,15 @@ type Props = {
   image: string,
 }
 
-function ProductCard({ id, title, price, image }: Props) {  
+const ProductCard = ({ id, title, price, image }: Props) => {  
   return (
       <div className="card">
           <Link to={"/products/" + id}><img src={image}></img></Link>
           <Link to={"/products/" + id}><h4>{title}</h4></Link>
           <p>${price.toFixed(2)}</p>
       </div>    
-    )
-  }
+  );
+}
   
-  export default ProductCard;
+export default ProductCard;
   
